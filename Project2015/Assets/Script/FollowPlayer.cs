f5c7cf467ccac230d91028bbe0a201668fc0f4e7
PaulSohn <paulsohn1998@naver.com>
2015-09-20 11:50:05 +0900

    change repo

---
+++ Project2015/Assets/Script/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Vector3 displacement;
+
+	private Transform player;
+	
+	void Awake () {
+		displacement=new Vector3(0,0,0);
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void FixedUpdate () {
+		transform.position=player.position+displacement;
+
+
+	}
+}
